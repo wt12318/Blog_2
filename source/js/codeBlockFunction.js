7051344d0a0cbb388cdcda4ed315f3b24334b586
wt12318 <1650464505@qq.com>
2022-03-29 23:57:56 +0800

    update

--- source/js/codeBlockFunction.js
+++ source/js/codeBlockFunction.js
@@ -1,4 +1,11 @@
-// 代码块功能依赖
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+function load_language() {
+    var blocks = $('code.hljs');
+    for (var i = 0; i < blocks.length; i++) {
+        var span = document.createElement('span');
+        span.classList.add('codeblock-language');
+        span.innerHTML = blocks[i].classList[1];
+        blocks[i].insertBefore(span, blocks[i].children[0]);
+    }
+}
+
+$(document).ready(load_language);
